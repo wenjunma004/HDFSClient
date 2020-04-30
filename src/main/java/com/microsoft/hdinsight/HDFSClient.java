c50d962f3ece44e048e6c2c353d9848bfbc89346
Your Name <you@example.com>
2020-04-30 10:57:54 -0700

    add more

--- src/main/java/com/microsoft/hdinsight/HDFSClient.java
+++ src/main/java/com/microsoft/hdinsight/HDFSClient.java
@@ -1,19 +1,40 @@
-gitpackage com.microsoft.hdinsight;
+package com.microsoft.hdinsight;
 
+import com.google.common.base.Strings;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.security.UserGroupInformation;
 
 
+import java.io.IOException;
 import java.net.URI;
+import java.security.PrivilegedExceptionAction;
 
 public class HDFSClient {
-    public static void main(String[] args) throws  Exception{
+    private final Configuration conf = new Configuration();
+    FileSystem fs;
+    private UserGroupInformation ugi;
+    public HDFSClient() throws IOException, InterruptedException {
+        conf.addResource("core-site.xml");
+        conf.addResource("hdfs-site.xml");
+       //fs = FileSystem.get(new URI("hdfs://localhost:8020"), new Configuration(), "hdfs");
+        fs = FileSystem.get(conf);
+        ugi = UserGroupInformation.getCurrentUser();
 
-        FileSystem fs = FileSystem.get(new URI("hdfs://localhost:8020"), new Configuration(), "hdfs");
+        fs = execute(new PrivilegedExceptionAction<FileSystem>() {
+            public FileSystem run() throws IOException {
+                System.out.println("run it...");
+                return FileSystem.get(conf);
+            }
+        });
+
+
+    }
+
+    public void listFiles() throws IOException {
         FileStatus[] statuses = fs.listStatus(new Path("/"));
-
         for(FileStatus status:statuses){
             if(status.isFile()){
                 System.out.println("---file---");
@@ -24,5 +45,47 @@ public class HDFSClient {
                 System.out.println(status.getPath());
             }
         }
+
+
+    }
+    public static void main(String[] args) throws  Exception{
+        HDFSClient hdfsClient = new HDFSClient();
+        hdfsClient.listFiles();
+
+    }
+
+    /**
+     * Executes action on HDFS using doAs
+     * @param action strategy object
+     * @param <T> result type
+     * @return result of operation
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public <T> T execute(PrivilegedExceptionAction<T> action)
+            throws IOException, InterruptedException {
+        T result = null;
+
+        // Retry strategy applied here due to HDFS-1058. HDFS can throw random
+        // IOException about retrieving block from DN if concurrent read/write
+        // on specific file is performed (see details on HDFS-1058).
+        int tryNumber = 0;
+        boolean succeeded = false;
+        do {
+            tryNumber += 1;
+            try {
+                result = ugi.doAs(action);
+                succeeded = true;
+            } catch (IOException ex) {
+                if (!Strings.isNullOrEmpty(ex.getMessage()) && !ex.getMessage().contains("Cannot obtain block length for")) {
+                    throw ex;
+                }
+                if (tryNumber >= 3) {
+                    throw ex;
+                }
+                Thread.sleep(1000);  //retry after 1 second
+            }
+        } while (!succeeded);
+        return result;
     }
 }
