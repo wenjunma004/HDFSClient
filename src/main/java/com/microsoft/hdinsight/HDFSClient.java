da81414bf055ae986c1d13a61c0b1e846977a465
Your Name <you@example.com>
2020-04-30 09:37:53 -0700

    update ip

--- src/main/java/com/microsoft/hdinsight/HDFSClient.java
+++ src/main/java/com/microsoft/hdinsight/HDFSClient.java
@@ -1,4 +1,4 @@
-package com.microsoft.hdinsight;
+gitpackage com.microsoft.hdinsight;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
@@ -11,16 +11,16 @@ import java.net.URI;
 public class HDFSClient {
     public static void main(String[] args) throws  Exception{
 
-        FileSystem fs = FileSystem.get(new URI("hdfs://10.160.254.62:8020"), new Configuration(), "hdfs");
+        FileSystem fs = FileSystem.get(new URI("hdfs://localhost:8020"), new Configuration(), "hdfs");
         FileStatus[] statuses = fs.listStatus(new Path("/"));
 
         for(FileStatus status:statuses){
             if(status.isFile()){
-                System.out.println("---fine---");
+                System.out.println("---file---");
                 System.out.println(status.getPath());
                 System.out.println(status.getLen());
             }else{
-                System.out.println("----folder info--------");
+                System.out.println("----folder info --------");
                 System.out.println(status.getPath());
             }
         }
